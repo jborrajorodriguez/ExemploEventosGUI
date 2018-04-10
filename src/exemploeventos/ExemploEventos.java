8b6738cd72fe33ce3d05fa83e857b060f0b91c44
Juan Borrajo Rodriguez <juanborrajo10@gmail.com>
2018-04-10 09:39:48 +0200

    Main

---
+++ src/exemploeventos/ExemploEventos.java
@@ -0,0 +1,14 @@
+package exemploeventos;
+
+/**
+ *
+ * @author Juan Borrajo Rodriguez
+ */
+public class ExemploEventos {
+
+    
+    public static void main(String[] args) {
+        Evento nuevo=new Evento();
+    }
+
+}
